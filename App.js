09113fe750ded86fb622e8c8ae2879ce5022ef40
Jouni Juntunen <jouni.juntunen@oamk.fi>
2022-01-30 11:20:36 +0200

    Finished example.

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState,useEffect } from 'react'; 
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function App() {
+  const [screenOrientation, setScreenOrientation] = useState('portrait');
+  const [isPortrait, setIsPortrait] = useState(true);
+
+  useEffect(() => {
+    const subscription = ScreenOrientation.addOrientationChangeListener((value) => {
+    if (value.orientationInfo.orientation === ScreenOrientation.Orientation.PORTRAIT_UP) {
+      setScreenOrientation("portrait");
+      setIsPortrait(true);
+    } else if (value.orientationInfo.orientation === ScreenOrientation.Orientation.LANDSCAPE_RIGHT) {
+      setScreenOrientation("landscape");
+      setIsPortrait(false);
+    }
+    });
+    return () => {
+      ScreenOrientation.removeOrientationChangeListener(subscription);
+    };
+  
+  }, []);
+
+ 
+  const lockToPortrait = async () => {
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container,isPortrait ? styles.portrait : styles.landscape]}>
+      <Text>{screenOrientation}</Text>
+
+      {(() => {
+        if (isPortrait) {
+          return <Text>This content is only for portrait</Text>
+        } else {
+          return <Text>This content is only for landscape</Text>
+        }
+      })()}
+
+     {/*  {isPortrait ===true && 
+        <Text>This content is only for portrait</Text>
+      } */}
+      <Button onPress={lockToPortrait} title="Lock to portrait"/>
     </View>
   );
 }
@@ -17,4 +54,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  portrait: {
+    backgroundColor: '#ccc',
+  },
+  landscape: {
+    backgroundColor: '#999',
+  }
 });
